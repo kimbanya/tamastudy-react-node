56f15c854274a388f2656d25ee8c72009536f782
jonsoku2 <whdtjr2792@gmail.com>
2020-03-31 08:28:47 +0900

    [react] chore: 환경변수파일처리

--- jongseok/client/src/keys.js
+++ jongseok/client/src/keys.js
@@ -1,2 +1,1 @@
-export const GOOGLE_MAP_KEY =
-  process.env.REACT_APP_GOOGLE_MAP_KEY || 'AIzaSyDVdwJ4597oGdNAZhHxO9rx2bcdTa6MHOc';
+export const GOOGLE_MAP_KEY = process.env.REACT_APP_GOOGLE_MAP_KEY || '';
